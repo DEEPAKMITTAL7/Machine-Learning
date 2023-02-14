9a43f1fcb33f3778d503eda87f630d6386a85564
DEEPAKMITTAL7 <dmittal9925.dm@gmail.com>
2023-02-14 13:41:16 +0530

    Project

---
+++ Projects/Major/Real State Price Prediction/server/server.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import util
+import json
+
+# %run util.ipynb
+
+app = Flask(__name__)
+
+
+@app.route('/hi')
+def hi():
+    return util.areas()
+
+
+@app.route('/locations')
+def locations():
+    response = jsonify({
+        'locations': util.locations()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    size = int(request.form['size'])
+    bath = int(request.form['bath'])
+    area_type = request.form['area_type']
+    balcony = int(request.form['balcony'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(area_type, location, size, total_sqft, bath, balcony)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+if __name__ == '__main__':
+    print('python flask started')
+    app.run()
